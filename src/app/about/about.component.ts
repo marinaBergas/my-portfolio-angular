6eaba1484a23d48dfa03ea0c0d33fb6d85ccdf3b
marinaBergas <65818690+marinaBergas@users.noreply.github.com>
2021-11-21 23:44:20 +0200

    update-progress

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  public skills = [
+    { skillName: 'HTML5', percentage: '95%' },
+    { skillName: 'css3', percentage: '92%' },
+    { skillName: 'Bootstrap', percentage: '98%' },
+    { skillName: 'javascript', percentage: '90%' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
