64934f61c1e47624ba8352017987a081d71550f9
marinaBergas <65818690+marinaBergas@users.noreply.github.com>
2021-11-25 20:18:02 +0200

    navbar-collapse

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,11 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  HostListener,
+  Inject,
+} from '@angular/core';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -12,6 +19,23 @@ export class NavbarComponent {
     this.scroll.emit(true);
   }
   constructor() {}
+
+  ngAfterContentInit() {
+    (() => {
+      let nav = document.getElementById('navbar') as HTMLElement;
+      let logo = document.querySelector('.logo') as HTMLElement;
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > 35) {
+          nav.classList.add('scroll');
+          logo.classList.add('logo-dark-color');
+        } else {
+          nav.classList.remove('scroll');
+          logo.classList.remove('logo-dark-color');
+        }
+      });
+      console.log('Listner added');
+    })();
+  }
 
   ngOnInit(): void {}
 }
