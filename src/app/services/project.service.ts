488f60383af30887f90d2e718c8c2fab2703d4a3
marinaBergas <65818690+marinaBergas@users.noreply.github.com>
2021-11-26 22:19:32 +0200

    fix-projectimage-path

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -9,10 +9,10 @@ export class ProjectService {
   private _projectList: Project[] = [
     {
       id: 1,
-      logoImg: `../../../assets/calmaLogo.png`,
+      logoImg: `../../assets/calmaLogo.png`,
       projectType: ProjectType.react,
       url: `${'https://marinabergas.github.io/Calma/#/'}`,
-      imgscreenshot: `../../../assets/calmascreenshot.png`,
+      imgscreenshot: `../../assets/calmascreenshot.png`,
       title: 'Calma Psychological Website',
       description:
         ' A Psychological booking website built with React, Redux,Material-UI, and Firebase',
@@ -32,7 +32,7 @@ export class ProjectService {
       logoImg: ``,
       projectType: ProjectType.react,
       url: `${'https://marinabergas.github.io/TodoList/'}`,
-      imgscreenshot: `../../../assets/TodoList-screenshot.png`,
+      imgscreenshot: `../../assets/TodoList-screenshot.png`,
       title: 'Todo website app',
       description:
         'Todo website app using react fake api deploy with github use hooks ,props',
@@ -42,7 +42,7 @@ export class ProjectService {
       logoImg: ``,
       projectType: ProjectType.react,
       url: `${'https://marinabergas.github.io/post-leave-request/#/'}`,
-      imgscreenshot: `../../../assets/post-leave-request.png`,
+      imgscreenshot: `../../assets/post-leave-request.png`,
       title: 'Post Leave Request',
       description: 'post leave request website using,react,hooks,redux',
     },
@@ -51,7 +51,7 @@ export class ProjectService {
       logoImg: ``,
       projectType: ProjectType.landingPage,
       url: `${'https://marinabergas.github.io/map-feature/#/'}`,
-      imgscreenshot: `../../../assets/school-mapscreenshot.png`,
+      imgscreenshot: `../../assets/school-mapscreenshot.png`,
       title: 'School map ',
       description:
         'react website  using leafletmap library carousel with javascript',
