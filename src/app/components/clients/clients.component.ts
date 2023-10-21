90c44a2e273c6ea67d44cd0139c8ccb09fea1a14
marina saber <marina.saber.ext@orange.com>
2023-10-21 19:11:39 +0300

    fix add images at images folder

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -11,25 +11,25 @@ export class ClientsComponent implements OnInit {
     {
       clientOpinion:
         'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.',
-      img: 'assets/client1.webp',
+      img: 'assets/images/client1.webp',
       name: ' Jeremy Winston',
     },
     {
       clientOpinion:
         'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.',
-      img: 'assets/client2.webp',
+      img: 'assets/images/client2.webp',
       name: '  Richard Atkinson',
     },
     {
       clientOpinion:
         'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.',
-      img: 'assets/client3.webp',
+      img: 'assets/images/client3.webp',
       name: ' Jeremy Winston',
     },
     {
       clientOpinion:
         'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.',
-      img: 'assets/client4.webp',
+      img: 'assets/images/client4.webp',
       name: '  Richard Atkinson',
     },
   ];
